11b3404894255f746b704cf515ccd6719612ce93
charlie-black <boompiccasso@gmail.com>
2024-03-28 05:52:57 +0300

    changed the loader color

--- lib/components/shared_resources/widgets.dart
+++ lib/components/shared_resources/widgets.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import '../../utils/constants.dart';
 
 
 
 Widget buildLoader() {
-  return Center(child: SpinKitPulse(color: kBackgroundColor));
+  return const Center(child: SpinKitPulse(color: Colors.black));
 }
 
 
