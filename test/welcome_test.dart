7e7dfcf6a8e32ce2499215ea54b2e57e2a47490e
charlie-black <boompiccasso@gmail.com>
2024-03-29 21:26:37 +0300

    tests

--- test/welcome_test.dart
+++ test/welcome_test.dart
@@ -1,5 +1,6 @@
 import 'package:ecom_101/screens/home_page.dart';
 import 'package:ecom_101/screens/welcome.dart';
+import 'package:ecom_101/shared_resources/gradient_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -15,5 +16,21 @@ void main() {
     await tester.pumpAndSettle();
 
     expect(find.byType(HomePage), findsOneWidget);
+  });
+
+  testWidgets('GradientButton widget test', (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: GradientButton(
+            text: 'Test Button',
+            onPressed: () {},
+            width: 200,
+          ),
+        ),
+      ),
+    ));
+
+    expect(find.text('Test Button'), findsOneWidget);
   });
 }
