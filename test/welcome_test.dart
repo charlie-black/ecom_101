4ed938782d45bcafb8f3df75e352c18ce05d450d
charlie-black <boompiccasso@gmail.com>
2024-03-29 21:29:09 +0300

    tests

--- test/welcome_test.dart
+++ test/welcome_test.dart
@@ -5,18 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('WelcomeScreen widget test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MaterialApp(
-      home: WelcomeScreen(),
-    ));
-
-    expect(find.text('Welcome to GemStore!'), findsOneWidget);
-
-    await tester.tap(find.text('Get Started'));
-    await tester.pumpAndSettle();
-
-    expect(find.byType(HomePage), findsOneWidget);
-  });
 
   testWidgets('GradientButton widget test', (WidgetTester tester) async {
     await tester.pumpWidget(MaterialApp(
