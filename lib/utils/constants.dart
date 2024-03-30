1a898700fefb26059afe09d08d2d894d83d1fd68
charlie-black <boompiccasso@gmail.com>
2024-03-30 12:41:37 +0300

    removed unused constants

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -4,11 +4,7 @@ import 'package:hexcolor/hexcolor.dart';
 
 final Color kPrimaryTextColor = HexColor("#FFFFFF");
 final Color kBackgroundColor = HexColor("#FFFFFF");
-final Color kIconColor = HexColor("#4caf50");
-final Color kActiveColor = HexColor('#852e9f');
 final Color kDangerColor = HexColor('#f44335');
-final Color kDividerColor = HexColor('#778096');
-final Color kCardColor = HexColor('#27293c');
 const kDefaultPadding = 8.0;
 
 
